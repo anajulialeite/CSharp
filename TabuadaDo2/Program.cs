74109f2b6498841460799a78f0ce6b69ba4e2cf7
Ana Júlia Leite <139260194+anajulialeite@users.noreply.github.com>
2023-07-18 21:40:07 -0300

    Tabuado do 2
    Aprendendo a fazer multiplicação

---
+++ TabuadaDo2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabuadaDo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Tabuada do 2");
+            Console.Write("2 x 1 = ");
+            Console.WriteLine(2 * 1);
+            Console.Write("2 x 2 = ");
+            Console.WriteLine(2 * 2);
+            Console.Write("2 x 3 = ");
+            Console.WriteLine(2 * 3);
+            Console.Write("2 x 4 = ");
+            Console.WriteLine(2 * 4);
+            Console.Write("2 x 5 = ");
+            Console.WriteLine(2 * 5);
+            Console.Write("2 x 6 = ");
+            Console.WriteLine(2 * 6);
+            Console.Write("2 x 7 = ");
+            Console.WriteLine(2 * 7);
+            Console.Write("2 x 8 = ");
+            Console.WriteLine(2 * 8);
+            Console.Write("2 x 9 = ");
+            Console.WriteLine(2 * 9);
+            Console.Write("2 x 10 = ");
+            Console.WriteLine(2 * 10);
+            Console.ReadKey();
+            */
+            Console.WriteLine("2 x 1 = " +(2*1));
+            Console.WriteLine("2 x 2 = 4");
+            Console.WriteLine("...");
+            Console.WriteLine("2 x 10 = 20");
+            Console.ReadKey();
+        }
+    }
+}
