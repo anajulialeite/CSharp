5c00a4367e8849b3ea5615a2f27088583c021042
Ana Júlia Leite <139260194+anajulialeite@users.noreply.github.com>
2023-07-18 21:35:13 -0300

    Aula 01
    Aprendo a fazer textos!

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Ana Júlia de Lima Aguiar Leite");
+            Console.WriteLine("Bem-vindo ao curso de Programação C#");
+            Console.ReadLine();
+
+
+        }
+    }
+}
