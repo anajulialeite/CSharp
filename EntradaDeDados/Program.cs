84dc7820110ee070a0f2d01693538e77c6c160e5
Ana Júlia Leite <139260194+anajulialeite@users.noreply.github.com>
2023-07-18 22:37:08 -0300

    Entrada de dados
    Aprendendo a usar strings

---
+++ EntradaDeDados/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+
+            Console.WriteLine("Programa que efetua leitura de dados do usuário:");
+            Console.WriteLine("Olá, qual é o seu nome?");
+            Console.Write("Digite seu nome: ");
+            Console.ReadLine();
+            nome = Console.ReadLine();
+            //Console.WriteLine(nome+", Bem vinda as aulas de programação");
+            Console.WriteLine("{0} bem vindo as aulas de programação", nome);
+            Console.ReadKey();
+
+        }
+    }
+}
