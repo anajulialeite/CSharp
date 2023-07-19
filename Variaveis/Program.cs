c26c6cab150bc8916f1c4122b69429656f85e6e4
Ana Júlia Leite <139260194+anajulialeite@users.noreply.github.com>
2023-07-18 22:06:39 -0300

    Variáveis
    Uma pequena demonstração de variáveis.

---
+++ Variaveis/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //tipo nome valor
+            string texto;
+            texto = "Variável";
+            Console.WriteLine(texto);
+            texto = "Ana Júlia de Lima Aguiar Leite";
+            Console.WriteLine(texto);
+            Console.ReadKey();*/
+
+            int n1 = 2;
+            int n2 = 4;
+
+            Console.WriteLine(n1 + " > " + n2 + " = " + (n1 > n2));
+            Console.WriteLine(n1 + " < " + n2 + " = " + (n1 < n2));
+            Console.WriteLine(n1 + " != " + n2 + " = " + (n1 != n2));
+            Console.WriteLine(n1 + " == " + n2 + " = " + (n1 == n2));
+            Console.ReadKey();
+
+
+        }
+    }
+}
