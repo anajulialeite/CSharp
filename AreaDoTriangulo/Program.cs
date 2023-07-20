577fe6a30076a886c2faa60a6d87b08ad6c6a792
Ana Júlia Leite <139260194+anajulialeite@users.noreply.github.com>
2023-07-20 14:45:42 -0300

    Calculando a área de um triângulo
    O usuário escolhe os valores e o programa calcula sozinho!

---
+++ AreaDoTriangulo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaDoTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0; //área
+            double b = 0; //base
+            double h = 0; //altura
+            string entrada = "";
+
+            Console.WriteLine("Área de um triângulo");
+            Console.WriteLine("Informe a base do triângulo: ");
+            entrada = Console.ReadLine();
+            b = Convert.ToDouble(entrada);
+            
+            Console.WriteLine("Informe a altura do triângulo: ");
+            entrada = Console.ReadLine();
+            h = Convert.ToDouble(entrada);
+
+            a = (b * h) / 2;
+
+            Console.WriteLine("Área de um triângulo");
+            Console.WriteLine("Base " + b);
+            Console.WriteLine("Altura " + h);
+            Console.WriteLine("Área " + a);
+            Console.ReadKey();
+        }
+    }
+}
