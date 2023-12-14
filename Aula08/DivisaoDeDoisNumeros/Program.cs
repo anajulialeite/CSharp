754f2b8404d22e3bddc9b6368536357a8d3c6c02
Ana Júlia Leite <139260194+anajulialeite@users.noreply.github.com>
2023-12-14 13:36:58 -0300

    Add files via upload

---
+++ Aula08/DivisaoDeDoisNumeros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisaoDeDoisNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = 0; //Número que será dividido
+            double n2 = 0; //Divisor
+            double r = 0; //Resto da divisão
+            double q = 0; //Quociente da divisão
+
+            Console.WriteLine("Programa que efetua a divisão de dois números");
+            Console.Write("Informe o primeiro número: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe o segundo número: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            q = n1 / n2;
+            r = n1 % n2;
+
+            Console.WriteLine("Resposta da divisão: ");
+            Console.WriteLine("{0}/{1}={2}", n1, n2, q);
+            Console.WriteLine("Resto: {0}", r);
+            Console.ReadKey();
+        }
+    }
+}
