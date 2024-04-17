bd3cf7e8fbe1f2a0f220af10e7ba5af12fb28a54
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-04-17 01:54:56 -0300

    Add files via upload

---
+++ Aula17/Media/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Media
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tl = 0; //quantidade de números 
+            double num = 0; //número informado pelo usuário
+            double total = 0; //soma dos números 
+            double media = 0; //média dos números
+            int i = 0; //contador do for
+
+            // O Double consegue representar o número 2x maior que o Float
+
+            Console.WriteLine("Programa que calcula a média dos números: ");
+            Console.Write("Quantos números deseja informar: ");
+            tl = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= tl; i++)
+            {
+                Console.Write("informe um número: ");
+                num = Convert.ToDouble(Console.ReadLine());
+                //total = total + num;
+                total += num;
+            }
+            media = num / tl;
+            Console.WriteLine("Total: "+total);
+            Console.WriteLine("Média: {0}",media);
+            Console.ReadKey();
+        }
+        
+    }
+}
